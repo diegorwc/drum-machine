dfc24a01a574d01b01fd6cb5da844f756fa5baef
Diego <wille.diegoricardo@gmail.com>
2022-06-16 21:18:23 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -4,23 +4,56 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
+  
+  const playAudio = (id) => {
+    console.log(document.getElementById(id))  
+    document.getElementById(id).play()
+  }
+
   return (
     <div className="App" id='drum-machine'>
       <div id='display'>
         <div className='row'>
-          <div className='drum-pad col-4' id='drum-q'>Q</div>
-          <div className='drum-pad col-4' id='drum-w'>W</div>
-          <div className='drum-pad col-4' id='drum-e'>E</div>
+          <div className='drum-pad col-4 btn btn-success' id='drum-q' onClick={playAudio("Q")}>            
+            Q
+            <audio id="Q" className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
+          </div>
+          <div className='drum-pad col-4 btn btn-success' id='drum-w'>
+            W
+            <audio id="W" className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
+          </div>
+          <div className='drum-pad col-4 btn btn-success' id='drum-e'>
+            E
+            <audio id="E" className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
+          </div>
         </div>
         <div className='row'>
-          <div className='drum-pad col-4' id='drum-a'>A</div>
-          <div className='drum-pad col-4' id='drum-s'>S</div>
-          <div className='drum-pad col-4' id='drum-d'>D</div>
+          <div className='drum-pad col-4 btn btn-success' id='drum-a'>
+            A
+            <audio id="A" className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
+          </div>
+          <div className='drum-pad col-4 btn btn-success' id='drum-s'>
+            S
+            <audio id="S" className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>  
+          </div>
+          <div className='drum-pad col-4 btn btn-success' id='drum-d'>
+            D
+            <audio id="D" className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
+          </div>
         </div>
         <div className='row'>
-          <div className='drum-pad col-4' id='drum-z'>Z</div>
-          <div className='drum-pad col-4' id='drum-x'>X</div>
-          <div className='drum-pad col-4' id='drum-c'>C</div>
+          <div className='drum-pad col-4 btn btn-success' id='drum-z'>
+            Z
+            <audio id="Z" className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>  
+          </div>
+          <div className='drum-pad col-4 btn btn-success' id='drum-x'>
+            X
+            <audio id="X" className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>  
+          </div>
+          <div className='drum-pad col-4 btn btn-success' id='drum-c'>
+            C
+            <audio id="C" className="clip" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
+          </div>
         </div>
       </div>
     </div>
